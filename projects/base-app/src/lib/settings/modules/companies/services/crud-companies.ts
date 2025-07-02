1a5b36e7442162efe8d2dca4488cfb082e752792
Elias123Gonzalez <tec4@avalantec.com>
2025-07-02 12:52:36 -0600

    better hard coded example

--- projects/base-app/src/lib/settings/modules/companies/services/crud-companies.ts
+++ projects/base-app/src/lib/settings/modules/companies/services/crud-companies.ts
@@ -13,16 +13,44 @@ export class CrudCompanies extends ApiRequestManager {
   getCompanies(): company[] {
     return [
       {
-        _id: 'abc',
-        name: 'ACB Company',
+        _id: 'c1a2b3',
+        name: 'TechNova Solutions',
       },
       {
-        _id: 'cde',
-        name: 'CDE Company',
+        _id: 'c2b3c4',
+        name: 'GreenCore Industries',
       },
       {
-        _id: '123',
-        name: 'Avalantec Company',
+        _id: 'c3c4d5',
+        name: 'QuantumSoft',
+      },
+      {
+        _id: 'c4d5e6',
+        name: 'BluePeak Technologies',
+      },
+      {
+        _id: 'c5e6f7',
+        name: 'SolarEdge Corp',
+      },
+      {
+        _id: 'c6f7g8',
+        name: 'Nimbus Analytics',
+      },
+      {
+        _id: 'c7g8h9',
+        name: 'Oceanix Global',
+      },
+      {
+        _id: 'c8h9i0',
+        name: 'EcoSphere Ventures',
+      },
+      {
+        _id: 'c9i0j1',
+        name: 'FutureWorks Inc.',
+      },
+      {
+        _id: 'c0j1k2',
+        name: 'Vertex Dynamics',
       },
     ];
   }
