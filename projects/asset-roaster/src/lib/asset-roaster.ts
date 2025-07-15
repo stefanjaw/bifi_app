65085676cd90977cf1c635c4650c768092c81f67
Elias123Gonzalez <tec4@avalantec.com>
2025-07-15 11:28:04 -0600

    Asset rooster lib init
    Co-authored-by: SebasUC <seulloaca@users.noreply.github.com>

---
+++ projects/asset-roaster/src/lib/asset-roaster.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-asset-roaster',
+  imports: [],
+  template: `
+    <p>
+      asset-roaster works!
+    </p>
+  `,
+  styles: ``
+})
+export class AssetRoaster {
+
+}
