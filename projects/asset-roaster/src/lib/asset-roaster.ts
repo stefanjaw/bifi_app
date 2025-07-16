9afa48d4001731fdc1bd2204ce2097a7c5be3f01
Elias123Gonzalez <tec4@avalantec.com>
2025-07-16 17:19:22 -0600

    removed unused files

--- projects/asset-roaster/src/lib/asset-roaster.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-asset-roaster',
-  imports: [],
-  template: ` <p>asset-roaster works!</p> `,
-  styles: ``,
-})
-export class AssetRoaster {}
