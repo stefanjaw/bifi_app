4ec11e5f193d32d59aa4ea5905b330b4199c4de2
Sebastian Ulloa <xdpsebas@gmail.com>
2025-07-22 09:46:26 -0600

    Create table library

--- projects/base-app/form/src/index.ts
+++ projects/base-app/form/src/index.ts
@@ -1,15 +1,3 @@
-import { FormActions } from './components/form-actions/form-actions';
-import { FormError } from 'projects/base-app/form/src/components/form-error/form-error';
-import { FormField } from 'projects/base-app/form/src/components/form-field/form-field';
-import { FormLabel } from 'projects/base-app/form/src/components/form-label/form-label';
-import { FormLayout } from 'projects/base-app/form/src/components/form-layout/form-layout';
-import { FormNavigator } from 'projects/base-app/form/src/components/form-navigator/form-navigator';
-import { FormSection } from 'projects/base-app/form/src/components/form-section/form-section';
-import { Hint } from 'projects/base-app/form/src/components/hint/hint';
-import { FormActionsHandler } from 'projects/base-app/form/src/directives/form-actions-handler';
-import { FormControlExtension } from 'projects/base-app/form/src/directives/form-control-extension';
-import { TranslatedErrors } from 'projects/base-app/form/src/directives/translated-errors';
-
 export * from './components/form-actions/form-actions';
 export * from './components/form-error/form-error.model';
 export * from './components/form-error/form-error';
@@ -19,6 +7,7 @@ export * from './components/form-layout/form-layout';
 export * from './components/form-navigator/form-navigator';
 export * from './components/form-section/form-section';
 export * from './components/hint/hint';
+export * from './components';
 export * from './directives/form-actions-handler';
 export * from './directives/form-control-extension';
 export * from './directives/translated-errors';
@@ -35,17 +24,3 @@ export * from './services/form-field-context';
 export * from './services/form-sections';
 export * from './services/form-translation';
 export * from './services/typed-form-builder';
-
-export const AppFormExtensionsImports = [
-  FormLayout,
-  FormNavigator,
-  FormSection,
-  FormError,
-  FormField,
-  FormLabel,
-  Hint,
-  FormControlExtension,
-  TranslatedErrors,
-  FormActions,
-  FormActionsHandler,
-];
