99a94d59113a071b6f84d9765e78a92a2e0c42a8
Elias123Gonzalez <tec4@avalantec.com>
2025-08-20 11:09:52 -0600

    refactor: simplify settings menu structure and integrate sidenav manager

--- projects/base-app/settings/src/services/settings-menu-manager.ts
+++ projects/base-app/settings/src/services/settings-menu-manager.ts
@@ -1,32 +1,38 @@
-import { Injectable, signal } from '@angular/core';
-import { BaseMenuManager } from '@avalantec/base-app/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { BaseMenuManager, SidenavManager } from '@avalantec/base-app/core';
 import { MenuItem } from 'primeng/api';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SettingsMenuManager extends BaseMenuManager {
+  private sidenavManager = inject(SidenavManager);
+
   constructor() {
     const menuItems = signal<MenuItem[]>([
       {
-        icon: 'pi pi-question',
+        icon: 'pi pi-users',
         routerLink: ['/settings/contacts'],
         label: 'Contacts',
+        command: () => this.sidenavManager.closeSidenav(),
       },
       {
-        icon: 'pi pi-fw',
+        icon: 'pi pi-building',
         routerLink: ['/settings/companies'],
         label: 'Companies',
+        command: () => this.sidenavManager.closeSidenav(),
       },
       {
-        icon: 'pi pi-fw',
+        icon: 'pi pi-user',
         routerLink: ['/settings/roles'],
         label: 'Roles',
+        command: () => this.sidenavManager.closeSidenav(),
       },
       {
-        icon: 'pi pi-fw',
+        icon: 'pi pi-check',
         routerLink: ['/settings/permissions'],
-        label: 'Permissions',
+        label: 'Policies',
+        command: () => this.sidenavManager.closeSidenav(),
       },
     ]);
 
