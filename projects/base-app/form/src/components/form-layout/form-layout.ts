a516d9e564698f6bd5a05956b40b15192c8e7859
Elias123Gonzalez <tec4@avalantec.com>
2025-08-26 10:35:58 -0600

    Improved form layout title displaying

--- projects/base-app/form/src/components/form-layout/form-layout.ts
+++ projects/base-app/form/src/components/form-layout/form-layout.ts
@@ -1,14 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { Text, TextVariant } from '@avalantec/base-app/core';
 // import { FormNavigator } from '../form-navigator/form-navigator';
 
 @Component({
   selector: 'bifi-app-form-layout',
-  imports: [Text],
+  imports: [Text, CommonModule],
   templateUrl: './form-layout.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FormLayout {
   title = input.required<string>();
+  titlePosition = input<'left' | 'center' | 'right'>('left');
   titleVariant = input<TextVariant>('title-3');
 }
