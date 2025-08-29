d0f831ad8dabb0ffd476fcc599110a5050d8c663
Elias123Gonzalez <tec4@avalantec.com>
2025-08-29 14:18:44 -0600

    fixed imports

--- projects/asset-roaster/src/lib/modules/product-types/services/crud-product-types.ts
+++ projects/asset-roaster/src/lib/modules/product-types/services/crud-product-types.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ApiRequestManager } from '@avalantec/base-app/resource';
-import { productType } from 'projects/asset-roaster/src/public-api';
+import { productType } from '../interfaces/product-type';
 
 @Injectable({
   providedIn: 'root',
