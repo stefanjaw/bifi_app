1a2de3059117d0e8dfdd89d97b7185b45933f34e
Elias123Gonzalez <tec4@avalantec.com>
2025-09-16 12:40:27 -0600

    fix: refactor policy handling in select policy dialog to use computed properties

--- projects/base-app/settings/src/modules/roles/components/roles-form/select-policy-dialog/select-policy-dialog.ts
+++ projects/base-app/settings/src/modules/roles/components/roles-form/select-policy-dialog/select-policy-dialog.ts
@@ -1,10 +1,11 @@
-import { ChangeDetectionStrategy, Component, inject, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, output } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { BaseDialog, policy } from '@avalantec/base-app/core';
 import { FormModule } from '@avalantec/base-app/form';
 import { DialogModule } from 'primeng/dialog';
 import { SelectModule } from 'primeng/select';
 import { CrudPolicies } from '../../../../policies';
+import { RoleForm } from '../../../services/role-form';
 
 @Component({
   selector: 'bifi-app-select-policy-dialog',
@@ -14,12 +15,17 @@ import { CrudPolicies } from '../../../../policies';
 })
 export class SelectPolicyDialog extends BaseDialog {
   private readonly crudPolicies = inject(CrudPolicies);
+  private form = inject(RoleForm);
 
   private policyResource = this.crudPolicies.get({
     triggerRequest: this.dialogState, // Only trigger the request when the dialog is opened
   });
 
-  policies = this.policyResource.value;
+  policies = computed(() => {
+    const policies = this.policyResource.value();
+    return policies.filter(p => !this.form.value().policies?.some(pId => pId === p._id));
+  });
+  
   policy = new FormControl<string | null>(null);
   selected = output<policy<any, any>>();
 
