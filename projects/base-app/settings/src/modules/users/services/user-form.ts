62031ccc5e424458ce920aa7a4855af51addf3db
Elias123Gonzalez <tec4@avalantec.com>
2025-09-30 15:26:54 -0600

    Added selector to chose related contact in user's form

--- projects/base-app/settings/src/modules/users/services/user-form.ts
+++ projects/base-app/settings/src/modules/users/services/user-form.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Validators } from '@angular/forms';
 import { ArrayValidators, BaseForm } from '@avalantec/base-app/form';
 
 export interface UserFormModel {
@@ -6,6 +7,7 @@ export interface UserFormModel {
   email?: string;
   picture?: string;
   roles: string[];
+  contactId: string;
 }
 
 @Injectable({
@@ -22,6 +24,7 @@ export class UserForm extends BaseForm<UserFormModel> {
         validators: [ArrayValidators.minLength(1)],
         formArrayElements: [],
       },
+      contactId: ['', Validators.required],
     });
   }
 }
