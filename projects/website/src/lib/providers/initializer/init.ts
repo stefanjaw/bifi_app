ed67581c0c408f9d833070e03826f4d20024665d
Elias123Gonzalez <tec4@avalantec.com>
2025-12-09 09:50:17 -0600

    feat: add config library script and enhance website initialization with menu items

--- projects/website/src/lib/providers/initializer/init.ts
+++ projects/website/src/lib/providers/initializer/init.ts
@@ -1,6 +1,6 @@
 import { inject } from '@angular/core';
 import { WEBSITE_ROUTES } from '../../routes/website-routes';
-import { MainRoutingManager } from '@avalantec/base-app/routing';
+import { MainMenuManager, MainRoutingManager } from '@avalantec/base-app/routing';
 
 export function initializeWebsite() {
   initializeMenu();
@@ -8,6 +8,35 @@ export function initializeWebsite() {
 
 function initializeMenu() {
   const mainRoutingManager = inject(MainRoutingManager);
+  const mainMenuManager = inject(MainMenuManager);
+
+  mainMenuManager.addItems([
+    {
+      item: {
+        id: 'website',
+        label: 'Website',
+        icon: 'pi pi-globe',
+        routerLink: ['/website'],
+        showInMainMenu: true,
+      },
+    },
+    {
+      item: {
+        id: 'website',
+        label: 'Website',
+        icon: 'pi pi-globe',
+        items: [
+          {
+            icon: 'pi pi-file-pdf',
+            routerLink: ['/settings/templates'],
+            label: 'Templates',
+            resource: 'templates/menu',
+          },
+        ],
+      },
+      childOf: 'settings',
+    },
+  ]);
 
   mainRoutingManager.addRouting({
     newRouting: WEBSITE_ROUTES,
