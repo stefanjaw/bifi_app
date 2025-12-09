ea939d66e9e8e16c7bee02dbec14cf7e7af3a91c
Elias123Gonzalez <tec4@avalantec.com>
2025-12-09 14:39:02 -0600

    feat: enhance configuration scripts with improved logging and add styles.css for global styles

--- tools/config/config-library.sh
+++ tools/config/config-library.sh
@@ -7,18 +7,18 @@
 PARENT_DIR="$(CDPATH= cd -- "$(dirname -- "$0")/../../../" && pwd)"
 SUBMODULE_DIR="$(CDPATH= cd -- "$(dirname -- "$0")/../../" && pwd)"
 
-echo "Parent directory: $PARENT_DIR"
-echo "Submodule directory: $SUBMODULE_DIR"
+echo "\n 📁 Parent directory: $PARENT_DIR"
+echo "\n 📁 Submodule directory: $SUBMODULE_DIR"
 
 # ---------- INSTALL SUBMODULE DEPENDENCIES ----------
 
-echo "Installing submodule dependencies... on $SUBMODULE_DIR"
-npm install --prefix "$SUBMODULE_DIR" || { echo "Failed to install submodule dependencies"; exit 1; }
+echo "\n 📦 Installing submodule dependencies... on $SUBMODULE_DIR"
+npm install --prefix "$SUBMODULE_DIR" || { echo "\n ❌ Failed to install submodule dependencies"; exit 1; }
 
 # ---------- RUN BUILD SCRIPT TO GET SELECTED LIBRARIES ----------
 
 LIBS=$(sh "$SUBMODULE_DIR/tools/build/build.sh" "$@")
-echo "Selected libraries to config: $LIBS"
+echo "\n ✅ Selected libraries to config: $LIBS"
 
 # Check if user cancelled the build process
 if [ "$LIBS" = "Cancelled by user." ]; then
@@ -42,31 +42,30 @@ for LIB in $LIBS; do
         if [ -n "$TGZ_FILE" ]; then
             LIB_PATHS="$LIB_PATHS $TGZ_FILE"
         else
-            echo "Warning: No .tgz file found in $DIST_DIR for library $LIB"
+            echo "\n ⚠️ Warning: No .tgz file found in $DIST_DIR for library $LIB"
         fi
     else
-        echo "Warning: Distribution directory $DIST_DIR does not exist for library $LIB"
+        echo "\n ⚠️ Warning: Distribution directory $DIST_DIR does not exist for library $LIB"
     fi
 done
 
-echo "Library package paths: $LIB_PATHS"
-
+echo "\n 📦 Library package paths: $LIB_PATHS"
 # Install the libraries in the parent project
-echo "Installing libraries in parent project... on $PARENT_DIR"
+echo "\n 📦 Installing libraries in parent project... on $PARENT_DIR"
 
 # Install everything and then install the libraries in the parent project
 rm "$PARENT_DIR/package-lock.json"
-npm install --prefix "$PARENT_DIR" || { echo "Failed to install parent project dependencies"; exit 1; }
-npm install --prefix "$PARENT_DIR" $LIB_PATHS || { echo "Failed to install libraries in parent project"; exit 1; }
+npm install --prefix "$PARENT_DIR" || { echo "\n ❌ Failed to install parent project dependencies"; exit 1; }
+npm install --prefix "$PARENT_DIR" $LIB_PATHS || { echo "\n ❌ Failed to install libraries in parent project"; exit 1; }
 
 # ---------- UPDATE CONFIG FILES ----------
 
 # Configure environment variables for templates
-echo "\nInsert the credentials in ./src/environments/environment.ts"
+echo "\n 📝 Insert the credentials in ./src/environments/environment.ts"
 vi "$PARENT_DIR/src/environments/environment.ts"
 
 # Ask for title
-read -r -p "Enter the application title: " APP_TITLE
+read -r -p "\n 📝 Enter the application title: " APP_TITLE
 
 export APP_TITLE
 
@@ -76,13 +75,13 @@ generate_file() {
     local output="$2"
 
     if [ ! -f "$template" ]; then
-        echo "Template not found: $template"
+        echo "\n ❌ Template not found: $template"
         return 1
     fi
 
     envsubst < "$template" > "$output"
 
-    echo "Generated: $output"
+    echo "\n ✅ Generated: $output"
 }
 
 # Set the routes file path, if not created, create it
@@ -99,6 +98,11 @@ PRIMENG_PATH="$CONFIG_DIR/primeng.preset.ts"
 [ ! -f "$PRIMENG_PATH" ] && touch "$PRIMENG_PATH"
 generate_file "$SUBMODULE_DIR/tools/config/primeng.preset.ts.txt" "$PRIMENG_PATH"
 
+# Generate styles.css
+STYLES_PATH="$PARENT_DIR/src/styles.css"
+[ ! -f "$STYLES_PATH" ] && touch "$STYLES_PATH"
+generate_file "$SUBMODULE_DIR/tools/config/styles.css.txt" "$STYLES_PATH"
+
 # Generate app.html and app.ts
 HTML_PATH="$CONFIG_DIR/app.html"
 TS_PATH="$CONFIG_DIR/app.ts"
@@ -111,6 +115,7 @@ generate_file "$SUBMODULE_DIR/tools/config/app.ts.txt" "$TS_PATH"
 
 # Calculate providers based on selected libraries
 PROVIDERS=""
+PROVIDER_IMPORTS=""
 SEP=""
 
 # Iterate over selected libraries to extract provider names
@@ -121,24 +126,27 @@ for LIB in $LIBS; do
         PROVIDER_NAME=$(grep -E 'export (const|function) provide' "$PROVIDER_FILE" | head -n 1 | awk '{print $3}' | tr -d ':=')
 
         if [ -n "$PROVIDER_NAME" ]; then
-            PROVIDERS="$PROVIDERS$SEP$PROVIDER_NAME"
+            PROVIDERS="$PROVIDERS$SEP\n$PROVIDER_NAME"
+            PROVIDER_IMPORTS="$PROVIDER_IMPORTS\nimport { $PROVIDER_NAME } from '@avalantec/$LIB';"
             SEP=", "
         else
-            echo "Warning: No provider found in $PROVIDER_FILE"
+            echo "\n ⚠️ Warning: No provider found in $PROVIDER_FILE"
         fi
     else
-        echo "Warning: Provider file does not exist: $PROVIDER_FILE"
+        echo "\n ⚠️ Warning: Provider file does not exist: $PROVIDER_FILE"
     fi
 done
 
 export PROVIDERS
+export PROVIDER_IMPORTS
 
 # Generate app.config.ts
 CONFIG_PATH="$CONFIG_DIR/app.config.ts"
 [ ! -f "$CONFIG_PATH" ] && touch "$CONFIG_PATH"
 generate_file "$SUBMODULE_DIR/tools/config/app.config.ts.txt" "$CONFIG_PATH"
 
-echo "Providers ADDED: $PROVIDERS"
+echo "\n ✅ Providers ADDED: $PROVIDERS"
+echo "\n ✅ Provider IMPORTS: $PROVIDER_IMPORTS"
 
 # ---------- BUILDING PARENT PROJECT ----------
 echo "\nBuilding parent project..."
