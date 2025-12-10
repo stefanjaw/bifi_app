1203834fc9b04eeda54dc797ffd036535f1b5401
Elias123Gonzalez <tec4@avalantec.com>
2025-12-10 16:12:36 -0600

    feat: implement reactive validation for createdType and createdMake fields in product form

--- projects/asset-roster/src/lib/modules/products/services/create-product-form.ts
+++ projects/asset-roster/src/lib/modules/products/services/create-product-form.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { effect, Injectable } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { Validators } from '@angular/forms';
 import { BaseForm } from '@avalantec/base-app/form';
 
@@ -19,6 +20,34 @@ export interface CreateProductFormModel {
 
 @Injectable({ providedIn: 'root' })
 export class CreateProductForm extends BaseForm<CreateProductFormModel> {
+  productType = toSignal(this.form.controls.productTypeIds.valueChanges);
+  make = toSignal(this.form.controls.makeIds.valueChanges);
+
+  constructor() {
+    super();
+
+    effect(() => {
+      if (!this.productType()) {
+        this.form.controls.createdType.get('name')?.setValidators([Validators.required]);
+        this.form.controls.createdType.get('description')?.setValidators([Validators.required]);
+      } else {
+        this.form.controls.createdType.get('name')?.clearValidators();
+        this.form.controls.createdType.get('description')?.clearValidators();
+      }
+
+      this.form.controls.createdType.get('name')?.updateValueAndValidity();
+      this.form.controls.createdType.get('description')?.updateValueAndValidity();
+
+      if (!this.make()) {
+        this.form.controls.createdMake.get('oemName')?.setValidators([Validators.required]);
+      } else {
+        this.form.controls.createdMake.get('oemName')?.clearValidators();
+      }
+
+      this.form.controls.createdMake.get('oemName')?.updateValueAndValidity();
+    });
+  }
+
   override createForm() {
     return this.fb.group<CreateProductFormModel>({
       productTypeIds: [undefined!],
