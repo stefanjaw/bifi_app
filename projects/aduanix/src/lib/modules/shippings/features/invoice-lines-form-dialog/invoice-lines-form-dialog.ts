f30aadab04aa410a3df1167ae6d527b63286f8f0
Elias123Gonzalez <tec4@avalantec.com>
2025-12-30 16:30:30 -0600

    Update

--- projects/aduanix/src/lib/modules/shippings/features/invoice-lines-form-dialog/invoice-lines-form-dialog.ts
+++ projects/aduanix/src/lib/modules/shippings/features/invoice-lines-form-dialog/invoice-lines-form-dialog.ts
@@ -1,5 +1,11 @@
-
-import { ChangeDetectionStrategy, Component, DestroyRef, inject, output, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  DestroyRef,
+  inject,
+  output,
+  signal,
+} from '@angular/core';
 import { DialogModule } from 'primeng/dialog';
 import { BaseDialog, Text } from '@avalantec/base-app/core';
 import { FormModule } from '@avalantec/base-app/form';
@@ -7,11 +13,16 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'bifi-app-invoice-lines-form-dialog',
-  imports: [DialogModule, ReactiveFormsModule,FormModule, Text],
+  imports: [DialogModule, ReactiveFormsModule, FormModule, Text],
   templateUrl: './invoice-lines-form-dialog.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-
 export class InvoiceLinesFormDialog extends BaseDialog {
+  //   protected formService = inject(InvoiceLinesForm);
+  //   private crudInvoiceLine = inject(CrudInvoiceLine);
+  //   form = this.formService.form;
 
+  submitLoading = signal<boolean>(false);
+  destroy$ = inject(DestroyRef);
+  shippingCreated = output<void>();
 }
