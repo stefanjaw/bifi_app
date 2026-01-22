9ef5044190fbf1f38e7ceb8ac30be8418c30b406
Kim-Uva <kimfh1996@gmail.com>
2026-01-22 10:25:37 -0600

    feat: update contact columns to use fullName and fullAddress, remove unused fields

--- projects/base-app/contacts/src/libraries/contact-columns.ts
+++ projects/base-app/contacts/src/libraries/contact-columns.ts
@@ -3,17 +3,12 @@ import { tableColumn } from '@avalantec/base-app/resource';
 
 export const contactColumns: tableColumn<contact>[] = [
   {
-    field: 'name',
+    field: 'fullName',
     title: 'Contact Name',
     type: 'text',
     sortable: true,
   },
-  {
-    field: 'lastName',
-    title: 'Last Name',
-    type: 'text',
-    sortable: true,
-  },
+
   {
     field: 'phoneNumber',
     title: 'Phone Number',
@@ -49,39 +44,13 @@ export const contactColumns: tableColumn<contact>[] = [
     type: 'text',
     parseField: (row: contact[]) => row?.map(child => child.name).join(', ') || 'Not set',
   },
+
+
   {
-    field: 'countryId.name',
-    title: 'Country',
-    type: 'text',
-  },
-  {
-    field: 'state',
-    title: 'State',
+    field: 'fullAddress',
+    title: 'Address',
     type: 'text',
     sortable: true,
-  },
-  {
-    field: 'city',
-    title: 'City',
-    type: 'text',
-    sortable: true,
-  },
-  {
-    field: 'zipCode',
-    title: 'Zip Code',
-    type: 'text',
-    sortable: true,
-  },
-  {
-    field: 'streetAddress',
-    title: 'Street Address',
-    type: 'text',
-    sortable: true,
-  },
-  {
-    field: 'streetAddress2',
-    title: 'Street Address 2',
-    type: 'text',
-    sortable: true,
-  },
+  }
+  
 ];
