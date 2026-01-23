188f60e7397c617b9102f27e7dc0e7bda6b48abc
Elias123Gonzalez <tec4@avalantec.com>
2026-01-23 14:06:53 -0600

    Update

--- projects/base-app/users/src/components/user-profile/user-profile.ts
+++ projects/base-app/users/src/components/user-profile/user-profile.ts
@@ -124,7 +124,7 @@ export class UserProfile {
 
     const data = {
       contactInformation: {
-        _id: this.user()?.contactId?._id || '',
+        _id: this.user()?.contactId?._id || undefined,
         name: value.name,
         lastName: value.lastName,
         phoneNumber: value.phoneNumber,
