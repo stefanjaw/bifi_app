71bc9d518ab274725aa58d478436098893ea51f0
Elias123Gonzalez <tec4@avalantec.com>
2026-03-21 13:07:38 -0600

    Removed repeated module

--- projects/crm/src/lib/modules/crm-stages/libraries/crm-stage-filters.ts
+++
@@ -1,9 +0,0 @@
-import { filter } from '@avalantec/base-app/resource';
-import { crmStage } from '../interfaces/crm-stage';
-
-export const crmStageFilters: filter<crmStage>[] = [
-  {
-    field: 'name',
-    type: 'string',
-  },
-];
--- projects/crm/src/lib/providers/provider.ts
+++
@@ -1,6 +0,0 @@
-import { EnvironmentProviders, provideAppInitializer, Provider } from '@angular/core';
-import { initializeCrm } from './initializer/init';
-
-export function provideCrm(): EnvironmentProviders | Provider {
-  return [provideAppInitializer(initializeCrm)];
-}
